1dad0a2b2c76dc94bb7cd33cbf5c4f66954f3db6
eka Ponkratova <kate.ponkratova@gmail.com>
2025-02-08 19:48:47 +0800

    added homework

--- week_3/loading_data_s3_bq.sql
+++ week_3/loading_data_s3_bq.sql
@@ -8,5 +8,24 @@ select count(distinct PULocationID ) from  `zoomcamp.external_yellow_tripdata_no
 select count( PULocationID ) from  `zoomcamp.external_yellow_tripdata_non_partitoned` 
 where fare_amount = 0
 
--- Experimenting with partitions
-select * from  `zoomcamp.external_yellow_tripdata_non_partitoned` 
+--Checking size
+select PULocationID  from  `zoomcamp.external_yellow_tripdata_non_partitoned` 
+select PULocationID, DOLocationID  from  `zoomcamp.external_yellow_tripdata_non_partitoned` 
+
+-- Working with filters
+select distinct VendorID from `zoomcamp.external_yellow_tripdata_non_partitoned` 
+where date(tpep_dropoff_datetime)>='2024-03-01' and date(tpep_dropoff_datetime)<='2024-03-15'
+
+-- Materializing table
+CREATE OR REPLACE TABLE `zoomcamp.external_yellow_tripdata_non_partitoned_query`  AS
+select distinct VendorID from `zoomcamp.external_yellow_tripdata_non_partitoned` 
+where date(tpep_dropoff_datetime)>='2024-03-01' and date(tpep_dropoff_datetime)<='2024-03-15'
+
+--Creating partitioned table
+CREATE OR REPLACE TABLE `zoomcamp.external_yellow_tripdata_partitoned_query`
+PARTITION BY
+  DATE(tpep_dropoff_datetime) AS
+SELECT * FROM `zoomcamp.external_yellow_tripdata_non_partitoned`
+
+SELECT distinct VendorID FROM`zoomcamp.external_yellow_tripdata_partitoned_query`
+where date(tpep_dropoff_datetime)>='2024-03-01' and date(tpep_dropoff_datetime)<='2024-03-15'
